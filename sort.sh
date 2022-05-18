a2fae4fbaa24d25f02dd54c18bb8a2b44c0d7713
yasenoviy <85099827+yasenoviy@users.noreply.github.com>
2022-05-18 17:36:32 +0300

    Update sort.sh

--- sort.sh
+++ sort.sh
@@ -1,20 +1,50 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-echo "No parameters passed!"
-exit 1
-fi
+echo "Start sort settings"
+echo "Enter sort parametr: -u -up sort,  -d -down sort."
 
-if [ "$1" == "-u" ] || [ "$1" == "-d" ]; then
+while [ 1 ];
+do
+    read parametr
+    if [ $parametr == "-u" ] || [ $parametr == "-d" ];
+    then
+        if [ $parametr == "-u" ]; then
+            echo "Selected UP sort"
+        elif [ $parametr == "-d" ]; then
+            echo "Selected DOWN sort"
+        fi
+        break;
+    
+    elif [ $parametr != "-u" ] || [ $parametr != "-d" ];
+    then
+        echo "incorrect parametr"
+    fi
+done
+
+echo "Fill massive"
+
+mass=(0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0)
+
+for i in ${!mass[@]}; 
+do
+	echo "Enter $i's element"
+	read temp
+	mass[$i]=$temp
+done
+
+echo "Type: $parametr"
+echo "Massive: ${mass[@]}"
+
+
+if [ "$1" == "-d" ]; then
+echo "Type sort: DOWN"
+
+
+
+elif [ "$1" == "-u" ]; then
+echo "Type sort: UP"
 
 
 
 
-exit 0
-
-elif [ "$1" == "" ]; then
-echo "No sort type"
-
-elif [ "$1" != "-u" ] || [ "$1" != "-d" ]; then
-echo "Incorect sort type"
 fi
